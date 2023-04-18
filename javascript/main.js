8e9248cf8603faa774e4c7f505c835c1d5d3c68a
chiaravigi <vigilantechiara1@gmail.com>
2023-04-18 04:57:15 -0300

    the nav bg works responsively

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,10 @@
 const hamburgerIcon = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
+const navBg = document.querySelector(".show-nav");
 
 hamburgerIcon.addEventListener("click", () => {
   navMenu.classList.toggle("active");
-  hamburgerIcon.classList.toggle("active");
+  navBg.classList.toggle("active");
 });
 
 
